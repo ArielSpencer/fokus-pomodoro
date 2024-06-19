08fab60f7b9fea96269b3eaae47f1d9c139bc3c5
Ariel Spencer <arielspencer.tech@gmail.com>
2024-06-18 23:44:40 -0300

    Adicionar funcoes de deletar, cancelar e editar tarefas

--- script-crud.js
+++ script-crud.js
@@ -1,10 +1,27 @@
 // Selecionar elementos para adicionar tarefas
 const adicionarTarefaButton = document.querySelector('.app__button--add-task');
-const adicionarTarefaForm = document.querySelector('.app__form-add-task');
+const salvarTarefaForm = document.querySelector('.app__form-add-task');
+const cancelarTarefaForm = document.querySelector('.app__form-footer__button--cancel');
+const deletarTarefaForm = document.querySelector('.app__form-footer__button--delete');
 const textArea = document.querySelector('.app__form-textarea');
 const ulTarefas = document.querySelector('.app__section-task-list');
 
+// Armazenar no localStorage e converter para para uma string em formato JSON
 const listaDeTarefas = JSON.parse(localStorage.getItem('tarefas')) || []
+
+function atualizarTarefas() {
+    localStorage.setItem('tarefas', JSON.stringify(listaDeTarefas))
+}
+
+// Limpar textArea
+function limparTextArea() {
+    textArea.value = ''
+}
+
+// Esconder formulário
+function esconderForm() {
+    salvarTarefaForm.classList.add('hidden');
+}
 
 function criarElementoTarefa(tarefa) {
     const li = document.createElement('li')
@@ -31,6 +48,16 @@ function criarElementoTarefa(tarefa) {
     button.append(imagemButton)
     button.classList.add('app_button-edit')
 
+    button.onclick = () => {
+        const novaDescricao = prompt('Qual é o novo nome da tarefa?');
+
+        if (novaDescricao) {
+            paragrafo.textContent = novaDescricao;
+            tarefa.descricao = novaDescricao;
+            atualizarTarefas();
+        }
+    }
+
     li.append(svg);
     li.append(paragrafo);
     li.append(button);
@@ -38,13 +65,24 @@ function criarElementoTarefa(tarefa) {
     return li
 }
 
+// Ao clicar no botão deletar limpar textArea
+deletarTarefaForm.addEventListener('click', () => {
+    limparTextArea();
+})
+
+// Ao clicar no botão cancelar limpar textArea e ocultar formulário
+cancelarTarefaForm.addEventListener('click', () => {
+    esconderForm();
+    limparTextArea();
+})
+
 // Ao clicar no botão adicionar tarefa alternar a visibilidade do formulário
 adicionarTarefaButton.addEventListener('click', () => {
-    adicionarTarefaForm.classList.toggle('hidden')
+    salvarTarefaForm.classList.toggle('hidden')
 })
 
 // Prevenção do evento de submissão do form
-adicionarTarefaForm.addEventListener('submit', (evento) => {
+salvarTarefaForm.addEventListener('submit', (evento) => {
     evento.preventDefault();
     const tarefa = {
         descricao: textArea.value
@@ -54,11 +92,10 @@ adicionarTarefaForm.addEventListener('submit', (evento) => {
     // Mostrar tarefa na lista
     const elementoTarefa = criarElementoTarefa(tarefa);
     ulTarefas.append(elementoTarefa)
-    // Armazenar no localStorage e converter para para uma string em formato JSON
-    localStorage.setItem('tarefas', JSON.stringify(listaDeTarefas))
-    // Limpar textArea e esconder formulário
-    textArea.value = ''
-    adicionarTarefaForm.classList.add('hidden');
+
+    atualizarTarefas();
+    esconderForm();
+    limparTextArea();
 })
 
 listaDeTarefas.forEach(tarefa => {
